f15b819d1dec1ffab2cc715844388a3b09656a4b
Ambdul <ambdulanziz892@gmail.com>
2026-09-06 22:59:06 +0300

    PULL

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,8 @@
+#include "hal/gpio_types.h"
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <driver/gpio.h>
+#include <soc/gpio_num.h>
 #include <esp_log.h>
 
 namespace
@@ -10,5 +12,10 @@ namespace
 
 extern "C" void app_main()
 {
-    ESP_LOGI(TAG, "Hello GPIO TAG");
+    gpio_config_t led_config{};
+
+    led_config.pin_bit_mask = 1ULL << GPIO_NUM_7;
+    led_config.mode = GPIO_MODE_OUTPUT;
+    led_config.pull_up_en = GPIO_PULLUP_DISABLE;
+    led_config.pull_down_en = GPIO_PULLDOWN_DISABLE;
 }
